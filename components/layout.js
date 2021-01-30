4523368311faa5f1adf9d439e1170232d51b2ec3
guilleCM <guillermo.cirer@etoolinnovation.com>
2021-01-30 19:48:20 +0100

    add logo and slogan

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
 import 'antd/dist/antd.css'
 import { Layout } from 'antd';
+import styles from "./layout.module.css";
 
 const { Header, Footer, Content } = Layout;
 
@@ -7,7 +8,12 @@ const { Header, Footer, Content } = Layout;
 export default function PageLayout({ children }) {
     return (
         <Layout>
-            <Header>Header</Header>
+            <Header className={styles.appHeader}>
+                <img className={styles.logoHeader} src="/adoptallorca-inverse.png" alt="adoptallorca logo"></img>
+                <div className={styles.sloganHeader}>
+                    <img className={styles.sloganImg} src="/slogan-logo.png" alt="adoptallorca logo"></img>
+                </div>
+            </Header>
             <Content>{children}</Content>
             <Footer>Footer</Footer>
         </Layout>
